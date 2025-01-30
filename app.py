5fb3164da28625ffd914822d0d141407ed59a4d8
Alexandre André <alexandreandre2004@gmail.com>
2025-01-30 15:36:51 +0100

    m

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-   
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
+# Route pour servir sitemap.xml
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory('.', 'sitemap.xml', mimetype='application/xml')
 
 if __name__ == "__main__":
     app.run(debug=True)
